a2d07ac878425dca1c50d5c81a3cb2f632fa74cf
Anna-Koberidze <annakoberidze1@gmail.com>
2023-01-25 22:38:37 +0400

    working functional

--- src/App.js
+++ src/App.js
@@ -10,25 +10,16 @@ function App() {
     }));
   };
   let [saved, setSaved] = useState(0);
-  const [loaded, setLoaded] = useState(false);
-  const handleLoad = (e) => {
-    e.preventDefault();
-    setLoaded(true);
-  };
   const handleSave = (e) => {
     e.preventDefault();
     setSaved((saved = saved + 1));
     console.log("saved successfully");
   };
-  const unmountSave = (e) => {
-    e.preventDefault();
-    setSaved((saved = 0));
-  };
   useEffect(() => {
     if (saved > 0) {
       localStorage.setItem("active", JSON.stringify(active));
     }
-  }, [active]);
+  }, [saved]);
 
   useEffect(() => {
     const json = localStorage.getItem("active");
@@ -38,19 +29,16 @@ function App() {
         ...savedActive,
       }));
     }
-  }, [saved]);
+  }, []);
 
   return (
-    <div onLoad={handleLoad}>
+    <div>
       <h1>100 days of code challenge check off calendar</h1>
 
       <div className="App">
         <div className="button-container">
           <button className="note__save" onClick={handleSave}>
-            activate saving mode
-          </button>
-          <button className="unmount-save" onClick={unmountSave}>
-            deactivate saving mode
+            Save
           </button>
         </div>
         <div className="container">
