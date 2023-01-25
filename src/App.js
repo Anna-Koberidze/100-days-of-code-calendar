b8b7b2f5838c3d4deba4d5949bb424b504931c9e
Anna-Koberidze <annakoberidze1@gmail.com>
2023-01-25 21:44:31 +0400

    savable

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import numbers from "./numbers";
 function App() {
   const [active, setActive] = useState({});
@@ -9,8 +9,35 @@ function App() {
       [index]: !state[index],
     }));
   };
+  let [saved, setSaved] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const handleLoad = (e) => {
+    e.preventDefault();
+    setLoaded(true);
+  };
+  const handleSave = (e) => {
+    e.preventDefault();
+    setSaved((saved = saved + 1));
+    console.log("saved successfully");
+  };
+  useEffect(() => {
+    if (saved > 0) {
+      localStorage.setItem("active", JSON.stringify(active));
+    }
+  }, [active]);
+
+  useEffect(() => {
+    const json = localStorage.getItem("active");
+    const savedActive = JSON.parse(json);
+    if (savedActive) {
+      setActive(() => ({
+        ...savedActive,
+      }));
+    }
+  }, [saved]);
+
   return (
-    <div>
+    <div onLoad={handleLoad}>
       <h1>100 days of code challenge check off calendar</h1>
       <div className="App">
         <div className="container">
@@ -26,6 +53,9 @@ function App() {
               </div>
             );
           })}
+          <button className="note__save" onClick={handleSave}>
+            save
+          </button>
         </div>
       </div>
     </div>
